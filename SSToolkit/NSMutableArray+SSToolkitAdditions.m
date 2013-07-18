6b1a22bec776b19540613f15e5fe3842c6cefe1a
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2013-07-18 14:04:03 +0300

    Don't crash when shuffling empty arrays

--- SSToolkit/NSMutableArray+SSToolkitAdditions.m
+++ SSToolkit/NSMutableArray+SSToolkitAdditions.m
@@ -4,6 +4,7 @@
 
 - (void)shuffle
 {
+    if (self.count == 0) return;
     for (NSUInteger i = [self count] - 1; i > 0; i--) {
         [self exchangeObjectAtIndex:arc4random_uniform(i + 1)
                   withObjectAtIndex:i];
